50abbc2036d3fd5ff12ec5b59e81e2e117e7b1bd
Brian Hermosi <brian.hermosi@gmail.com>
2021-03-04 09:06:13 -0300

    404 error, galeria

--- 404.php
+++ 404.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Sitio web de la artista visual argentina Julia Pereyra contacto">
+    <meta name="description" content="Sitio web de la artista visual argentina Julia Pereyra">
     <meta name="keywords" content="arte, artista visual, Julia Pereyra, arte argentino contemporáneo, pintoras argentinas, obras de arte, argentinian artist, art" >
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <link rel="stylesheet" href="../css/estilos.css">
-    <title>Julia Pereyra | Arte | Contacto</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Julia Pereyra | Arte | Taller</title>
 </head>
 <body class="container">
     <header>
@@ -19,8 +19,8 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="../index.html">INICIO</a>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../index.html">INICIO </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="works.html">OBRA</a>
@@ -32,31 +32,29 @@
                         <a class="nav-link" href="expos.html">MUESTRAS</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="studio.html">TALLER</a>
+                        <a class="nav-link" href="studio.html">TALLER<span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="contact.html">CONTACTO <span class="sr-only">(current)</span></a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">CONTACTO</a>
                     </li>
                 </ul>
             </div>
         </nav>
     </header>
     <main class="container main-grid" >
-        <h1>CONTACTÉMONOS!</h1>
-        <div class="subtitle">
-            <h3>escribime por acá o mandame un md por IG</h3>
+        <div class="container-404">
+            <h1 class="nombre">ERROR 404</h1>
+            <P>Esta página no se encuentra disponible en estos momentos. Disculpa las molestias.</P>
+            <a href="index.html" class="btn" >HOME</a>
         </div>
-            <form id="form" class="topBefore">
-                <input id="name" type="text" placeholder="NOMBRE">
-                <input id="email" type="text" placeholder="E-MAIL">
-                <textarea id="message" type="text" placeholder="MENSAJE"></textarea>
-                <input id="submit" type="submit" value="VAMOS!">
-            </form>
     </main>
-    <footer class="foot-grid main-footer">
-            <p> Website designed by Brian Hermosí | Bs As | 2021 </p>
-    </footer>    
+
+    <footer class="main-footer foot-grid">
+        <p> Website designed by Brian Hermosí | Bs As | 2021 </p>
+    </footer>
+    <script src="js/menu.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
 </body>
 </html>
